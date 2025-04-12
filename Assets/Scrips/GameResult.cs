f94c73f438655c82f59d4cbee64168d196076793
HuuVu1609 <huuvu7758@gmail.com>
2025-04-13 00:45:46 +0700

    Luu truu ket qua choi

---
+++ Assets/Scrips/GameResult.cs
@@ -0,0 +1,8 @@
+[System.Serializable]
+public class GameResult
+{
+    public int previousCarrotCount;
+    public float previousTime;
+    public int currentCarrotCount;
+    public float currentTime;
+}
